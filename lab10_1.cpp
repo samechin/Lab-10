3b3686b5b9c362664fec7d0402131f594e9dea30
samechin <cj.krittayoch@gmail.com>
2025-12-23 15:36:56 +0700

    Lab 10 finished

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,26 +2,48 @@
 using namespace std;
 
 int main(){
-	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
+    char grade;
+    int student = 0;
+	int count[5] = {}; 
+	cout << "Please input grade of each student (A-F) or input 0 to exit." << endl;
 	do{
-		cout << "Student [" << "]: ";
-		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
-			//Do something
-		}else if(true) // if grade is B
-			//Do something
-		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
+		cout << "Student [" << student + 1 << "]: ";
+		cin >> grade; 
+		
+		if(grade == 'A'){
+		    count[0]++;
+		    student++;
+		    
+		}else if(grade == 'B'){
+		    count[1]++;
+		    student++;
+		    
+		}else if(grade == 'C'){
+		    count[2]++;
+		    student++;
+		    
+		}else if(grade == 'D'){
+		    count[3]++ ;
+		    student++;
+		    
+		}else if(grade == 'F'){
+		    count[4] ++;
+		    student++;
+		    
+		}else if(grade == '0'){
+		    break ;
+		}else{ 
+		    cout << "Wrong input. Please input again." << endl;
 		} 
 	}while(true);
 	
 	
-	cout << "In total ? students.";
-	cout << "A = " << count[0] <<", ";
-	cout << "B = " << count[1] <<", ";	
-	//	and so on ... for grade = C, D, F	
+	cout << "In total " << student << " students." << endl;
+	cout << "A = " << count[0] << ", ";
+	cout << "B = " << count[1] << ", ";	
+	cout << "C = " << count[2] << ", ";
+	cout << "D = " << count[3] << ", ";
+	cout << "F = " << count[4] << endl;
 	
 	return 0;
 }
--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,36 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    float sum = 0;
+    float sum_of_square = 0;
+    string textline;
+
+    ifstream source("score.txt");
+
+    while (getline(source, textline))
+    {
+        float value = stof(textline);
+        sum += value;
+        sum_of_square += value * value;
+        count++;
+    }
+
+    float mean = sum / count;
+    float variance = (sum_of_square / count) - (mean * mean);
+    float standard_deviation = sqrt(variance);
+
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << standard_deviation << "\n";
+
+    return 0;
+}
